352c0ef118ebfb58875f3bebd1200a808a4744e0
Jarvis Mercer - (AMSL) <ams.h4ck3r@gmail.com>
2025-08-30 23:45:57 +0330

    github ts updated

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,6 +1,11 @@
-// src/lib/github.ts
+import { Profile } from "next-auth";
+
 export async function getGitHubFeed(username: string) {
   const res = await fetch(`https://api.github.com/users/${username}/events/public`);
   if (!res.ok) throw new Error("Failed to fetch GitHub feed");
   return res.json();
+}
+
+export interface GitHubProfile extends Profile {
+  login: string;
 }
